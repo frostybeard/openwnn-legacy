fdb0a00c704af0c79dc6e1aab7ee360455b5d1b3
Smith William <william.smith@esolutions.de>
2017-01-19 13:41:22 +0100

    Remove generated binaries from git project
    Remove libs/, obj/, and bin/ from the git project

--- obj/local/armeabi/objs/WnnJpnDic/WnnJpnDic.o.d
+++
@@ -1,8 +0,0 @@
-/home/mariotaku/工程/OpenWnn-legacy/obj/local/armeabi/objs/WnnJpnDic/WnnJpnDic.o: \
- /home/mariotaku/工程/OpenWnn-legacy/jni/libwnnJpnDic/WnnJpnDic.c \
- /home/mariotaku/工程/OpenWnn-legacy/jni/libwnnJpnDic/../libwnnDictionary/include/nj_lib.h \
- /home/mariotaku/工程/OpenWnn-legacy/jni/libwnnJpnDic/../libwnnDictionary/include/njx_lib.h
-
-/home/mariotaku/工程/OpenWnn-legacy/jni/libwnnJpnDic/../libwnnDictionary/include/nj_lib.h:
-
-/home/mariotaku/工程/OpenWnn-legacy/jni/libwnnJpnDic/../libwnnDictionary/include/njx_lib.h:
--- obj/local/armeabi/objs/WnnZHCNDic/WnnZHCNDic.o.d
+++
@@ -1,8 +0,0 @@
-/home/mariotaku/工程/OpenWnn-legacy/obj/local/armeabi/objs/WnnZHCNDic/WnnZHCNDic.o: \
- /home/mariotaku/工程/OpenWnn-legacy/jni/libwnnZHCNDic/WnnZHCNDic.c \
- /home/mariotaku/工程/OpenWnn-legacy/jni/libwnnZHCNDic/../libwnnDictionary/include/nj_lib.h \
- /home/mariotaku/工程/OpenWnn-legacy/jni/libwnnZHCNDic/../libwnnDictionary/include/njx_lib.h
-
-/home/mariotaku/工程/OpenWnn-legacy/jni/libwnnZHCNDic/../libwnnDictionary/include/nj_lib.h:
-
-/home/mariotaku/工程/OpenWnn-legacy/jni/libwnnZHCNDic/../libwnnDictionary/include/njx_lib.h:
